b7c9eb1dd76fa2a6aa15d58e597793e4d574eac6
Richy Delaney(cube) <richdel1991@gmail.com>
2011-10-05 13:44:11 +0100

    A more sane thresholding algorithm

--- Vision/Lab1/Road Markings/road_markings.cpp
+++ Vision/Lab1/Road Markings/road_markings.cpp
@@ -40,6 +40,17 @@ int* generate_hist(IplImage* source) {
 };
 
 int get_threshold(int* hist) {
+	int i=0,sum=0, num=0;;
+	for(;i<256;i++){
+		if(hist[i] != 0){
+			sum += i;
+			num++;
+		}
+	}
+	return (int)((double)sum / num);
+};
+
+int get_threshold_wrong(int* hist) {
 	int i=0, max = 0, index = 0;
 	for(;i<256;i++){
 		if(hist[i] > max)
@@ -65,16 +76,18 @@ void select_white_points( IplImage* source, IplImage* result, int threshold)
 	int number_channels=source->nChannels;
 	unsigned char white_pixel[4] = {255,255,255,0};
 	cvZero( result );
-	int row=40;
+	int row=40,dec=50;
 	for(;row<source->height;row++) {
-		int col = 80;
-		for(;col<source->width-80;col++){
+		int col=dec;
+		for(;col<source->width-dec;col++){
 			//unsigned char curr_point = GETPIXELPTRMACRO( source, col, row, width_step, pixel_step );
 			unsigned char* curr_point = ((unsigned char *) source->imageData + (row)*(width_step) + (col));
 			if(*curr_point >= threshold){
 				PUTPIXELMACRO( result, col, row, white_pixel, width_step, pixel_step, number_channels );
 			}	
 		}
+		if(dec > 0)
+			dec --;
 	}
 
 	//// Apply morphological opening and closing operations to clean up the image
@@ -96,7 +109,6 @@ int main( int argc, char** argv )
  
     CvCapture* capture = cvCaptureFromAVI("StayingInLane.avi");
     int fps = (int) cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
-    printf("%d\n", fps);
 
     int user_clicked_key = 0;
     if(!cvGrabFrame(capture)) {
@@ -119,7 +131,7 @@ int main( int argc, char** argv )
 
     while(cvGrabFrame(capture) && user_clicked_key != ESC) {
 	    int * hist = generate_hist(tmp);
-	    int threshold = (double) get_threshold(hist) / 2;
+	    int threshold =  get_threshold(hist) + 20;
 	    img = cvRetrieveFrame(capture);
 	    tmp= cvCreateImage( cvSize(img->width, img->height), IPL_DEPTH_8U, 1);
 	    cvCvtColor(img, tmp, CV_RGB2GRAY);
