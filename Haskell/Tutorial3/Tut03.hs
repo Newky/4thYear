fcc5d164e43b5fac80a2606a80e758790e48ab96
Richy Delaney(cube) <richdel1991@gmail.com>
2011-10-22 17:09:32 +0100

    Slightly better version of exercise 1

--- Haskell/Tutorial3/Tut03.hs
+++ Haskell/Tutorial3/Tut03.hs
@@ -1,9 +1,16 @@
 -- Richard Delaney 08479950
 module Tut03 where
 
+import System.IO
+
 main :: IO ()
 main = do
-	contents <- getContents
+	calc "example.txt"
+
+calc :: FilePath -> IO ()
+calc path = do
+	handle <- openFile path ReadMode
+	contents <- hGetContents handle
 	putStr "Subtotals:\n" 
 	let x = map (\x -> foldr (+) 0 $ str2int $ words x ) $ lines contents
 	    total = sum x
