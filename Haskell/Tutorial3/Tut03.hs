7ec88b5afe148c681bf7293a6f637e776b29ceb8
Richy Delaney(cube) <richdel1991@gmail.com>
2011-10-22 16:49:38 +0100

    Some nice additions to the haskell exercise,
    More aligned with exactly what he wanted.

--- Haskell/Tutorial3/Tut03.hs
+++ Haskell/Tutorial3/Tut03.hs
@@ -1,14 +1,15 @@
 -- Richard Delaney 08479950
 module Tut03 where
 
+main :: IO ()
 main = do
 	contents <- getContents
 	putStr "Subtotals:\n" 
 	let x = map (\x -> foldr (+) 0 $ str2int $ words x ) $ lines contents
 	    total = sum x
-	putStr $ unwords $ map (\num -> "" ++ show num) x
+	putStr $ unwords $ map (\num -> show num) x
 	putStr "\nTotals:\n"
-	putStr $ "" ++ show total
+	putStrLn $ show total
 
 str2int :: [String] -> [Int]
 str2int [] = []
--- Haskell/Tutorial3/Tut03xcl.hs
+++ Haskell/Tutorial3/Tut03xcl.hs
@@ -1,30 +1,40 @@
 -- Richard Delaney 08479950
 module Tut03xcl where
+type Stack = [Int]
 
-main stack = do
+main :: IO ()
+main = xcl []
+
+xcl :: Stack -> IO b
+xcl stack = do
 	     putStrLn $ show $ stack
 	     inst <- getLine
-	     let num = head (words inst)
-             check num stack
+	     if null inst
+		then xcl stack
+		else let num = head (words inst)
+		     in check num stack
 
+check :: String -> Stack -> IO b
 check num stack
-	| num == "c" = main []
-	| isDigit num = main ( (read num::Int): stack)
+	| num == "c" = xcl []
+	| isDigit num = xcl ( (read num::Int): stack)
 	| isOperator num = checkop num stack
-	| otherwise = main stack
+	| otherwise = xcl stack
 
+checkop :: String -> Stack -> IO b
 checkop num stack
 	| num == "*" = doop (*) stack
 	| num == "+" = doop (+) stack
 	| num == "-" = doop (-) stack
-	
-doop f stack = main ((f op1 op2): stack')
+
+doop :: (Int -> Int -> Int) -> Stack -> IO b	
+doop f stack = xcl ((f op1 op2): stack')
 		where stack' = tail $ tail stack
 		      op1 = head(stack)
 		      op2 = head(tail(stack))
 
 isOperator :: String -> Bool
-isOperator x = elem x ["*","+"] 
+isOperator x = elem x ["*","+", "-"] 
 
 isDigit :: [Char] -> Bool
 isDigit (x:[]) = x >= '0' && x <= '9'
