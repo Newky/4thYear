3c871ac4d1987d36c4923f2256e4fea2de06e5be
Richy Delaney(cube) <richdel1991@gmail.com>
2011-10-25 10:06:33 +0100

    Added two tutorials together for submission

--- Haskell/Tutorial3/Tut03xcl.hs
+++ Haskell/Tutorial3/Tut03xcl.hs
@@ -1,9 +1,30 @@
 -- Richard Delaney 08479950
 module Tut03xcl where
+
+import System.IO
+
 type Stack = [Int]
 
+--Run Exercise 1
 main :: IO ()
 main = xcl []
+
+-- Run Exercise 2
+-- main = do
+-- 	calc "example.txt"
+
+calc :: FilePath -> IO ()
+calc path = do
+	handle <- openFile path ReadMode
+	contents <- hGetContents handle
+	let subs = map (\x -> foldr (+) 0 $ str2int $ words x ) $ lines contents
+	    total = sum subs
+	    subsstr = unwords $ map (\num -> show num) subs
+	putStrLn $ "Subtotals:\n" ++ subsstr ++ "\nTotal:\n" ++ show total
+
+str2int :: [String] -> [Int]
+str2int [] = []
+str2int (x:xs) = (read x::Int) : str2int xs
 
 xcl :: Stack -> IO b
 xcl stack = do
@@ -18,14 +39,14 @@ check :: String -> Stack -> IO b
 check num stack
 	| num == "c" = xcl []
 	| isDigit num = xcl ( (read num::Int): stack)
-	| isOperator num = checkop num stack
-	| otherwise = xcl stack
+	| otherwise = checkop num stack
 
 checkop :: String -> Stack -> IO b
-checkop num stack
-	| num == "*" = doop (*) stack
-	| num == "+" = doop (+) stack
-	| num == "-" = doop (-) stack
+checkop "*" stack = doop (*) stack
+checkop "+" stack = doop (+) stack
+checkop "-" stack = doop (-) stack
+checkop x stack = xcl stack
+
 
 doop :: (Int -> Int -> Int) -> Stack -> IO b	
 doop f stack 
