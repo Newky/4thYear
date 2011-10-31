2efabc67b388aa505276958386fcb9c82ef3219b
Richy Delaney(cube) <richdel1991@gmail.com>
2011-10-31 18:04:46 +0000

    Remembering some C++
    I'm out of touch...
    Made a word pair now.

--- FunCode/hash.cpp
+++ FunCode/hash.cpp
@@ -1,38 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-class Pair {
+class WordPair {
 	public:
+		WordPair(string, string);
+		~WordPair();
+		string random_item();
 		string word;
 		vector<string> matches;
 };
 
-class Dict {
-	private:
-		int hash(string key);
-		int size;
-		vector< vector<Pair> > values;
-	public:
-		Dict(int n);
-		~Dict();
-		string get(string key);
-		void put(string key, string value);
+WordPair::WordPair(string lh, string rh) {
+	this->word = lh;
+	this->matches = vector<string>();
+	this->matches.push_back(rh);
 };
 
-/* Constructor for Dict
- * Initialise with an array size of n
+string WordPair::random_item() {
+	int size = (int) this->matches.size();
+	int n = rand() % size;
+	string x = this->matches.at(n);
+	return x;
+};
+
+//class Dict {
+	//private:
+		//int hash(string key);
+		//int size;
+		//vector<WordPair> values[];
+	//public:
+		//Dict(int n);
+		//~Dict();
+		//string get(string key);
+		//void put(string key, string value);
+//};
+
+///* Constructor for Dict
+ //* Initialise with an array size of n
+ //*/
+//Dict::Dict(int n) {
+	//this->size = n;
+	////this->values = new vector<WordPair>[this->size];
+//};
+
+//Dict::~Dict() {
+//};
+
+/* To put an entry into the dictionary, we must 
+ * first hash the string, get out the array of
+ * vectors.
+ * Each vector from this vector (Sorry a bit cryptic)
+ * will hold a WordPair.
  */
-Dict::Dict(int n) {
-	this->size = n;
-	this->values.resize(this->size);
-};
-
-Dict::~Dict() {
-};
-
 //void Dict::put(string key, string value) {
 	//int x = hash(key);
 	//vector temp = this->values.begin();
@@ -42,7 +66,8 @@ Dict::~Dict() {
 //};
 
 int main() {
-
+	WordPair* x = new WordPair((string)"hello", (string)"world");
+	printf("%s", x->random_item().c_str());
 	return 0;
 }
 
